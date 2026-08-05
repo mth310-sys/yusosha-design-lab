726a3c305cdc0a5d29abe463890c34e144616e54
mth310-sys <mth.310@gmail.com>
2026-08-06 08:20:06 +0900

    ui.js を更新

--- JS/ui.js
+++ JS/ui.js
@@ -1,1 +1,20 @@
-(
+/* =========================================================
+   ui.js
+   UI取得
+========================================================= */
+
+const UI = {
+
+    topFrame : document.getElementById("top-frame"),
+
+    topHeight : document.getElementById("topHeight"),
+
+    topValue : document.getElementById("topValue"),
+
+    topSplit : document.getElementById("topSplit"),
+
+    editor : document.getElementById("editor"),
+
+    tabs : document.querySelectorAll(".tab")
+
+};
