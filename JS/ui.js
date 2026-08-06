ad6b11e4797bdeedfef25627a89e34b1a9306071
mth310-sys <mth.310@gmail.com>
2026-08-06 12:49:10 +0900

    ui.js を更新

--- JS/ui.js
+++ JS/ui.js
@@ -1,20 +1,46 @@
 /* =========================================================
    ui.js
-   UI取得
 ========================================================= */
 
 const UI = {
 
-    topFrame : document.getElementById("top-frame"),
+    topFrame :
+        document.getElementById("top-frame"),
 
-    topHeight : document.getElementById("topHeight"),
+    editor :
+        document.getElementById("editor"),
 
-    topValue : document.getElementById("topValue"),
-
-    topSplit : document.getElementById("topSplit"),
-
-    editor : document.getElementById("editor"),
-
-    tabs : document.querySelectorAll(".tab")
+    tabs :
+        document.querySelectorAll(".tab")
 
 };
+
+UI.tabs.forEach(tab=>{
+
+    tab.addEventListener("click",()=>{
+
+        UI.tabs.forEach(button=>
+            button.classList.remove("active")
+        );
+
+        tab.classList.add("active");
+
+        switch(tab.dataset.tab){
+
+            case "top":
+                showTopEditor();
+                break;
+
+            case "side":
+                UI.editor.innerHTML="<h3>Side編集</h3>";
+                break;
+
+            case "base":
+                UI.editor.innerHTML="<h3>Base編集</h3>";
+                break;
+
+        }
+
+    });
+
+});
